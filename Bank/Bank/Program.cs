099779d5a31517721822c5e1f71b9bb17b464766
eat-detey <premizova855@gmail.com>
2026-09-25 15:38:55 +0700

    #2 Добавлена функция отображения истории транзакций

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -13,7 +13,8 @@
 
             account1.MakeWithdrawal(123, DateTime.UtcNow, ":(");
             Console.WriteLine($"Balance: {account1.Balance}");
-             
+            
+            Console.WriteLine(account1.GetAccountHistory());
             try
             {
                 account2.MakeWithdrawal(10000, DateTime.UtcNow, "asdas");
